ebdb9ce4b833fe8e49f0c4e502c3bf557c0ad961
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-13 11:50:17 -0500

    update SecurityServiceImplementation in ticketing project rest

--- src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -1,31 +1,56 @@
 package com.cybertek.implementation;
 
+import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.User;
 import com.cybertek.entity.common.UserPrincipal;
+import com.cybertek.mapper.MapperUtil;
 import com.cybertek.repository.UserRepository;
 import com.cybertek.service.SecurityService;
+import com.cybertek.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
-    private UserRepository userRepository;
+    private UserService userService;
+    private MapperUtil mapperUtil;
 
-    public SecurityServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public SecurityServiceImpl(UserService userService, MapperUtil mapperUtil) {
+        this.userService = userService;
+        this.mapperUtil = mapperUtil;
     }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
-        User user = userRepository.findByUserName(s);
+        UserDTO userDTO= userService.findByUserName(s);
 
-        if(user==null){
+        if(userDTO==null){
             throw new UsernameNotFoundException("This user does not exists");
         }
 
-        return new UserPrincipal(user);
+        return new org.springframework.security.core.userdetails.User(userDTO.getId().toString(),
+                userDTO.getPassWord(),listAuthorities(userDTO));
+    }
+
+    @Override
+    public User loadUser(String value) {
+        UserDTO userDTO = userService.findByUserName(value);
+        return mapperUtil.convert(userDTO, new User());
+    }
+
+    private Collection<? extends GrantedAuthority> listAuthorities(UserDTO userDTO){
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(userDTO.getRole().getDescription());
+        authorityList.add(grantedAuthority);
+        return authorityList;
     }
 }
--- src/main/java/com/cybertek/service/SecurityService.java
+++ src/main/java/com/cybertek/service/SecurityService.java
@@ -1,5 +1,6 @@
 package com.cybertek.service;
 
+import com.cybertek.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,5 +9,8 @@ public interface SecurityService extends UserDetailsService {
 
     @Override
     UserDetails loadUserByUsername(String s) throws UsernameNotFoundException;
+
+
+    User loadUser(String value);
 
 }
