6817a5dd502e9776e88b81848b0a674dd8f8881e
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-17 19:23:52 -0500

    update enable globalmethod security ticketing project rest

--- src/main/java/com/cybertek/config/WebSecurityConfig.java
+++ src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -1,61 +1,56 @@
 package com.cybertek.config;
 
+import com.cybertek.filter.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-//we created this class to enable security(it has 4 steps)
-//
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    private SecurityFilter securityFilter;
 
+    @Autowired
+    private SecurityFilter securityFilter;
 
-//we created below bean for doing authentication without a form
-//(by using below bean i will create a method to handle authentication)
-//we were doing this authentication with form login for UI
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-    private static final String[] permittedUrls = {
+    private static final String[] permittedUrls ={
             "/authenticate",
-            "/create-user",
             "/confirmation",
+            "/api/p1/**",
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/configuration/security",
             "/swagger-ui.html",
             "/swagger-ui/**",
-            "/webjars/**"
+            "/webjars/**",
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
 
         http
-                .csrf()//in the middle somebody can attack if this is enabled
+                .csrf()
                 .disable()
                 .authorizeRequests()
-                //to get a token we put below endpoint for everybody(permitall)
                 .antMatchers(permittedUrls)
                 .permitAll()
                 .anyRequest()
                 .authenticated();
 
-//        http
-//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
 
     }
 }
