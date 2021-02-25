333ff3d46734c3f80782efdc48f0ee46c82026d8
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-24 19:50:41 -0500

    add performance aspect and annotation for execution time

--- src/main/java/com/cybertek/aspect/PerformanceAspect.java
+++ src/main/java/com/cybertek/aspect/PerformanceAspect.java
@@ -1,4 +1,10 @@
 package com.cybertek.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
 public class PerformanceAspect {
+
 }
